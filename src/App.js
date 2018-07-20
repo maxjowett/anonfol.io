f9183b7ec02f3141d83dc48726b005dcdb2b430c
maxjowett <jowett.max@gmail.com>
2018-07-20 13:37:29 -0700

    Begin code refactor, new create-react-app

--- src/App.js
+++ src/App.js
@@ -1,85 +1,18 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Searchbar from './components/Searchbar/Searchbar';
-import CoinDisplay from './components/CoinDisplay/CoinDisplay';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: '',
-      listings: null,
-      id: '',
-      coin: null
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.getMarket = this.getMarket.bind(this);
-    this.getId = this.getId.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      query: event.target.value
-    });
-  };
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.getMarket();
-  };
-
-  getMarket() {
-    let that = this;
-    fetch('https://api.coinmarketcap.com/v2/listings/')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      that.setState({
-        listings: data
-      }, () => { that.getId() });
-    })
-  };
-
-  getId() {
-    let that = this;
-    let query = that.state.query.toUpperCase();
-    for (let i = 0; i < that.state.listings.data.length; i++) {
-      if (that.state.listings.data[i].symbol == query) {
-        that.setState({
-          id: that.state.listings.data[i].id
-        }, () => { this.getCoin() });
-      }
-    }
-  };
-
-  getCoin() {
-    let that = this;
-    fetch(`https://cors-anywhere.herokuapp.com/http://api.coinmarketcap.com/v2/ticker/${this.state.id}`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      that.setState({
-        coin: data.data
-      });
-    })
-  };
-
   render() {
     return (
       <div className="App">
-        <Searchbar
-        query={this.state.query}
-        handleChange={this.handleChange}
-        handleSubmit={this.handleSubmit}
-        />
-        <CoinDisplay
-        query={this.state.query}
-        coin={this.state.coin}
-        />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
