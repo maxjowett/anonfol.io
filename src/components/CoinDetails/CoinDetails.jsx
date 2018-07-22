c04676c3c956d31bb4021ab6ddfaec569286cc48
maxjowett <jowett.max@gmail.com>
2018-07-21 23:26:18 -0700

    Add basic styling to coindetails component

--- src/components/CoinDetails/CoinDetails.jsx
+++ src/components/CoinDetails/CoinDetails.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './CoinDetails.css';
 
 class CoinDetails extends Component {
   constructor(props) {
@@ -9,18 +10,25 @@ class CoinDetails extends Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="twelve columns">
-            <div className="coin-details">
-              <img src={this.props.data.data.coins[0].iconUrl} width="64"></img>
-              <h2>{this.props.data.data.coins[0].name}</h2>
-              <p>{this.props.data.data.coins[0].symbol}</p>
-              <p>{this.props.data.data.coins[0].price}</p>
-              <p>{this.props.data.data.coins[0].rank}</p>
-              <p>{this.props.data.data.coins[0].marketCap}</p>
+        <div className="coin-details">
+            <div className="one column">
+              <img src={this.props.data.data.coins[0].iconUrl} width="32"></img>
+            </div>
+            <div className="three columns">
+              <h4 id="coin-details__coin-name">{this.props.data.data.coins[0].name}</h4>
+            </div>
+            <div className="three columns">
+              {this.props.data.data.coins[0].price}
+            </div>
+            <div className="two columns">
+              {this.props.data.data.coins[0].rank}
+            </div>
+            <div className="three columns">
+              {this.props.data.data.coins[0].marketCap}
+            </div>
             </div>
           </div>
         </div>
-      </div>
     );
   }
 }
