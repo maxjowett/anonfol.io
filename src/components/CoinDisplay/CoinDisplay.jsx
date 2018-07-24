484159b2fc8cde6a56bb3ff4c1a63551794b16af
maxjowett <jowett.max@gmail.com>
2018-07-24 15:29:02 -0700

    Include toLocaleString() method on all numeric values

--- src/components/CoinDisplay/CoinDisplay.jsx
+++ src/components/CoinDisplay/CoinDisplay.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+
+const numberWithCommas = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
 const CoinDisplay = (props) => {
   return (
     props.queryResponse != null
@@ -8,11 +14,12 @@ const CoinDisplay = (props) => {
       <div className="coin-display">
         <h2>{props.queryResponse.data.coins[0].name}</h2>
         <div>{props.queryResponse.data.coins[0].symbol}</div>
-        <div>{props.queryResponse.data.coins[0].price}</div>
-        <div>{props.queryResponse.data.coins[0].marketCap}</div>
-        <div>{props.queryResponse.data.coins[0].circulatingSupply}</div>
-        <div>{props.queryResponse.data.coins[0].totalSupply}</div>
+        <div>{props.queryResponse.data.coins[0].price.toLocaleString()}</div>
+        <div>{props.queryResponse.data.coins[0].marketCap.toLocaleString()}</div>
+        <div>{props.queryResponse.data.coins[0].circulatingSupply.toLocaleString()}</div>
+        <div>{props.queryResponse.data.coins[0].totalSupply.toLocaleString()}</div>
         <div>{props.queryResponse.data.coins[0].description}</div>
+        <div>{props.queryResponse.data.coins[0].change}</div>
       </div>
     </div>
     : null
