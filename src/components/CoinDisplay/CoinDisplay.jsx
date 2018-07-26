ff90473c0e7270a2700edd58bec4252d0c63ef75
maxjowett <jowett.max@gmail.com>
2018-07-25 23:13:05 -0700

    Remove skeleton.css, add misc number formatting functions

--- src/components/CoinDisplay/CoinDisplay.jsx
+++ src/components/CoinDisplay/CoinDisplay.jsx
@@ -1,65 +1,37 @@
 import React from 'react';
 import './CoinDisplay.css';
 
+const formatPrice = (str) => {
+  let value = '$' + parseFloat(str).toFixed(2);
+  return value;
+};
+
+const formatSymbol = (str) => {
+  let value = '(' + str + ')';
+  return value;
+};
+
+const formatChange = (str) => {
+  let value = str + '%';
+  return value;
+};
+
 const CoinDisplay = (props) => {
   return (
     props.queryObject != null &&
       <div className="coin-display">
-        <div className="row">
-          <div className="row">
-            <div className="one columns">
-              <img src={props.queryObject.iconUrl} width="64"></img>
-            </div>
-            <div className="eleven columns">
-              <div className="row">
-                <div className="twelve columns">
-                  <div className="coin-display__name">{props.queryObject.name}</div>
-                  <div className="coin-display__price">{props.queryObject.price.toLocaleString()}</div>
-                </div>
-                <div className="row">
-                  <div className="twelve columns">
-                    <div className="coin-display__symbol">{props.queryObject.symbol}</div>
-                    <div className="coin-display__change">{props.queryObject.change}</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-          <div className="row">
-            <div className="four columns">
-              Rank
-              <br />
-              <div className="coin-display__rank">{props.queryObject.rank}</div>
-            </div>
-            <div className="four columns">
-              <div className="coin-display__marketCap">{props.queryObject.marketCap.toLocaleString()}</div>
-            </div>
-            <div className="four columns">
-              <div className="coin-display__volume">{props.queryObject.volume.toLocaleString()}</div>
-            </div>
-          </div>
-        <div>{props.queryObject.circulatingSupply.toLocaleString()}</div>
-        <div>{props.queryObject.totalSupply.toLocaleString()}</div>
-        <div>{props.queryObject.description}</div>
+        <img src={props.queryObject.iconUrl} width="64"></img>
+        <div className="coin-display__name">{props.queryObject.name}</div>
+        <div className="coin-display__price">{formatPrice(props.queryObject.price)}</div>
+        <div className="coin-display__symbol">{formatSymbol(props.queryObject.symbol)}</div>
+        <div className="coin-display__change">{formatChange(props.queryObject.change)}</div>
+        <div className="coin-display__rank">{props.queryObject.rank}</div>
+        <div className="coin-display__marketCap">{props.queryObject.marketCap}</div>
+        <div className="coin-display__volume">{props.queryObject.volume}</div>
+        <div className="coin-display__circulatingSupply">{props.queryObject.circulatingSupply}</div>
+        <div className="coin-display__totalSupply">{props.queryObject.totalSupply}</div>
+        <div className="coin-display__description">{props.queryObject.description}</div>
       </div>
-    </div>
   );
 }
 
