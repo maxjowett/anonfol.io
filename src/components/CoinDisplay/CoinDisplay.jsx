469709515d38630eed82fac5684242f6729a571c
maxjowett <jowett.max@gmail.com>
2018-07-25 17:49:42 -0700

    Begin implementing css grid on coindisplay component

--- src/components/CoinDisplay/CoinDisplay.jsx
+++ src/components/CoinDisplay/CoinDisplay.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import './CoinDisplay.css';
 
 const CoinDisplay = (props) => {
   return (
     props.queryObject != null &&
-    <div className="g-row">
       <div className="coin-display">
-        <h2>{props.queryObject.name}</h2>
+        <div className="row">
+            <div className="one columns">
+              <img src={props.queryObject.iconUrl} width="64"></img>
+            </div>
+            <div className="eleven columns">
+              <div className="row">
+                <div className="twelve columns">
+                  <div className="coin-display__name">{props.queryObject.name}</div>
+                  <div className="coin-display__price">{props.queryObject.price.toLocaleString()}</div>
+                </div>
+            </div>
+          </div>
         <div>{props.queryObject.symbol}</div>
-        <div>{props.queryObject.price.toLocaleString()}</div>
         <div>{props.queryObject.marketCap.toLocaleString()}</div>
         <div>{props.queryObject.circulatingSupply.toLocaleString()}</div>
         <div>{props.queryObject.totalSupply.toLocaleString()}</div>
