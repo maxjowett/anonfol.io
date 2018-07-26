29d5fc1f5ed1084f77f88dcdd653a22cd7aa188f
maxjowett <jowett.max@gmail.com>
2018-07-25 19:39:53 -0700

    Continue improving styling on coindisplay - css grid issues remain

--- src/components/CoinDisplay/CoinDisplay.jsx
+++ src/components/CoinDisplay/CoinDisplay.jsx
@@ -6,6 +6,7 @@ const CoinDisplay = (props) => {
     props.queryObject != null &&
       <div className="coin-display">
         <div className="row">
+          <div className="row">
             <div className="one columns">
               <img src={props.queryObject.iconUrl} width="64"></img>
             </div>
@@ -15,14 +16,48 @@ const CoinDisplay = (props) => {
                   <div className="coin-display__name">{props.queryObject.name}</div>
                   <div className="coin-display__price">{props.queryObject.price.toLocaleString()}</div>
                 </div>
+                <div className="row">
+                  <div className="twelve columns">
+                    <div className="coin-display__symbol">{props.queryObject.symbol}</div>
+                    <div className="coin-display__change">{props.queryObject.change}</div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        <div>{props.queryObject.symbol}</div>
-        <div>{props.queryObject.marketCap.toLocaleString()}</div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+          <div className="row">
+            <div className="four columns">
+              Rank
+              <br />
+              <div className="coin-display__rank">{props.queryObject.rank}</div>
+            </div>
+            <div className="four columns">
+              <div className="coin-display__marketCap">{props.queryObject.marketCap.toLocaleString()}</div>
+            </div>
+            <div className="four columns">
+              <div className="coin-display__volume">{props.queryObject.volume.toLocaleString()}</div>
+            </div>
+          </div>
         <div>{props.queryObject.circulatingSupply.toLocaleString()}</div>
         <div>{props.queryObject.totalSupply.toLocaleString()}</div>
         <div>{props.queryObject.description}</div>
-        <div>{props.queryObject.change}</div>
       </div>
     </div>
   );
