b1485a8c718ea40c32d1020508a8457736e7c547
maxjowett <jowett.max@gmail.com>
2018-10-29 16:57:33 -0700

    Add api call on componentwillmount

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      coins: null
+    }
+    this.getCoins = this.getCoins.bind(this);
+  }
+
+  componentWillMount() {
+    this.getCoins()
+  }
+
+  getCoins() {
+    let that = this;
+    fetch('https://api.coinranking.com/v1/public/coins')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(res) {
+      res.status === 'success' && that.setState({ coins: res })
+    });
+  }
+
+  
   render() {
     return (
       <div className="App">
