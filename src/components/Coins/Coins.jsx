b41d634b0878e13cab2e7be499bc32c4eba3bd5f
maxjowett <jowett.max@gmail.com>
2018-10-29 19:55:02 -0700

    Render coin name and price on componentwillmount

--- src/components/Coins/Coins.jsx
+++ src/components/Coins/Coins.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import './Coins.css';
 
 class Coins extends Component {
   render(props) {
     return (
     <div>
       {
-        this.props.apiSuccess && this.props.coins.data.coins.map(coin => <div>{coin.name}</div>)
+        this.props.apiSuccess && 
+          this.props.coins.data.coins.map(coin =>
+            <div className="coin">
+              <div className="coin__name">{coin.name}</div>
+              <div className="coin__price">{coin.price}</div>
+            </div>
+          )
       }
     </div>
     );
