c34ab0b5e7f315e6df0b0908f33fdfdb9c37cc74
maxjowett <jowett.max@gmail.com>
2018-11-05 20:16:32 -0800

    Misc cleanup in App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Input from './components/Input/Input';
 
+// const APIKey = process.env.REACT_APP_COINMARKETCAP_API_KEY;
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
     }
   }
-  
+
   render() {
-    console.log(process.env.REACT_APP_COINMARKETCAP_API_KEY)
     return (
       <div className="App">
         <Input />
