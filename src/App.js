4c24a0fa606cfb089083578572b783e274122221
maxjowett <jowett.max@gmail.com>
2018-11-05 17:44:51 -0800

    Add .env file to store coinmarketcap api key

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import Coins from './components/Coins/Coins';
+import Input from './components/Input/Input';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      coins: null,
-      apiSuccess: false
     }
-    this.getCoins = this.getCoins.bind(this);
-  }
-
-  componentWillMount() {
-    this.getCoins()
-  }
-
-  getCoins() {
-    let that = this;
-    fetch('https://api.coinranking.com/v1/public/coins')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(res) {
-      res.status === 'success' && that.setState({ coins: res, apiSuccess: true })
-    });
   }
   
   render() {
+    console.log(process.env.REACT_APP_COINMARKETCAP_API_KEY)
     return (
       <div className="App">
-        <Coins
-          coins={this.state.coins}
-          apiSuccess={this.state.apiSuccess}
-        />
+        <Input />
       </div>
     );
   }
