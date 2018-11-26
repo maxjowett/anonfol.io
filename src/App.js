1f19a704d4662d2c289c23390894d6e2261bbf70
maxjowett <jowett.max@gmail.com>
2018-11-25 18:13:39 -0800

    Add create-react-app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Input from './components/Input/Input';
-
-// const APIKey = process.env.REACT_APP_COINMARKETCAP_API_KEY;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    }
-  }
-
   render() {
     return (
       <div className="App">
-        <Input />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
