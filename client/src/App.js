06c87f1cf2b6977f1625cf6cd58c7dc7c844bdb2
maxjowett <jowett.max@gmail.com>
2019-04-01 18:33:57 -0700

    Add coin component + API call

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
+import Searchbar from './components/Searchbar/Searchbar';
+
 class App extends Component {
+
+  state = {
+    coins: null
+  }
+
+  componentDidMount() {
+    this.getCoins()
+  }
+
+  getCoins = () => {
+    axios.get('https://api.coinranking.com/v1/public/coins')
+      .then(response => {
+        const { status, data } = response;
+        if (status === 200) {
+          this.setState({ coins: data });
+        }
+      })
+  };
+
+  
+
   render() {
     return (
       <div className="App">
+        <Searchbar />
       </div>
     );
   }
---
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Searchbar extends Component {
+  render() {
+    return (
+      <div>
+        <input type='text' />
+      </div>
+    );
+  }
+}
+
+export default Searchbar;
