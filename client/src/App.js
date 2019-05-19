2e960018052360c92435908fc064c77cbf5decf8
maxjowett <jowett.max@gmail.com>
2019-05-18 21:27:34 -0700

    Add component skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,15 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
 
-import Searchbar from './components/Searchbar/Searchbar';
-
-class App extends Component {
-
-  state = {
-    coins: null
-  }
-
-  componentDidMount() {
-    this.getCoins()
-  }
-
-  getCoins = () => {
-    axios.get('https://api.coinranking.com/v1/public/coins')
-      .then(response => {
-        const { status, data } = response;
-        if (status === 200) {
-          this.setState({ coins: data });
-        }
-      })
-  };
-
-  
-
-  render() {
-    return (
-      <div className="App">
-        <Searchbar />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+    </div>
+  );
 }
 
 export default App;
