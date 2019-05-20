a8a2de77f924b6ffb3f8979f5c09a07473e46499
maxjowett <jowett.max@gmail.com>
2019-05-19 19:23:18 -0700

    Add scrape script

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 
 class App extends Component {
-  componentDidMount = () => {
-    this.getCoins();
-  };
-  getCoins = () => {
-    axios.get('https://api.coinranking.com/v1/public/coins')
-    .then(response => console.log(response))
-  };
+  // componentDidMount = () => {
+  //   this.getCoins();
+  // };
+  // getCoins = () => {
+  //   axios.get('https://api.coinranking.com/v1/public/coins')
+  //   .then(response => console.log(response))
+  // };
   render() {
     return (
       <div className="App">
--- server.js
+++ server.js
@@ -7,8 +7,15 @@ const PORT = 8080;
 
 app.get('/api/scrape', (req, res) => {
     axios.get('https://api.coinranking.com/v1/public/coins')
-    .then(response => console.log(response))
-    .then(res.send('Scape complete!'))
+    .then(response => {
+      const { status, data } = response;
+      if (status === 200) {
+        fs.appendFile('coins.json', JSON.stringify(data.data.coins), (err) => {
+          if (err) throw err;
+          console.log('The file has been saved!')
+        })
+      }
+    })
 });
 
 app.listen(PORT, () => {
