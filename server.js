1561de30eb6caddf48c3488c5a1ad9e6a50d4360
maxjowett <jowett.max@gmail.com>
2019-05-20 16:17:50 -0700

    Add promiseBuilder function to handle multiple axios requests

--- server.js
+++ server.js
@@ -5,18 +5,41 @@ const fs = require('fs');
 const app = express();
 const PORT = 8080;
 
+
 app.get('/api/scrape', (req, res) => {
-    axios.get('https://api.coinranking.com/v1/public/coins')
-    .then(response => {
-      const { status, data } = response;
-      if (status === 200) {
-        fs.appendFile('coins.json', JSON.stringify(data.data.coins), (err) => {
-          if (err) throw err;
-          console.log('The file has been saved!')
-        })
-      }
+  let offset = 0;
+  let promises = [];
+  const promiseBuilder = () => {
+    let url = 'https://api.coinranking.com/v1/public/coins?offset='
+    if (offset < 50) {
+      promises.push(axios.get(`${url}${offset}`));
+      offset += 50;
+      return promiseBuilder();
+    }
+    return promises;
+    }
+  Promise.all(promiseBuilder())
+  .then((response) => {
+    let master = [];
+      for (r of response) {
+        master.push(...r.data.data.coins);
+      };
+      master = master.map(coin => {
+        const { id, symbol, name, color, iconType, iconUrl } = coin;
+        return {
+          id,
+          symbol,
+          name,
+          color,
+          iconType,
+          iconUrl
+        }
+      });
+      const json = JSON.stringify(master, null, 2);
+      fs.writeFileSync('coins.json', json);
     })
-});
+    .catch(err => console.log(err));
+  });
 
 app.listen(PORT, () => {
   console.log(`🚀  Server is listening on ${PORT}`);
