672da4238ea5125cfba138453731439efe5a3165
maxjowett <jowett.max@gmail.com>
2019-05-21 16:16:00 -0700

    Add table for coins

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import AddCoin from './components/AddCoin/AddCoin';
+import CoinTable from './components/CoinTable/CoinTable';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
         <Navbar />
         <Header />
         <AddCoin />
+        <CoinTable />
       </div>
     );
   }
--- client/src/components/AddCoin/AddCoin.jsx
+++ client/src/components/AddCoin/AddCoin.jsx
@@ -8,23 +8,40 @@ import './AddCoin.css';
 class AddCoin extends Component {
   state = {
     optionSelected: null,
-    amount: null
+    amount: null,
+    coins: []
   };
 
   handleOnOptionChange = e => {
-    // console.log(e)
-    this.setState({ optionSelected: e.symbol })
-  }
+    this.setState({ optionSelected: e.symbol });
+  };
 
   handleOnAmountChange = e => {
-    this.setState({ amount: e.target.value })
-  }
+    this.setState({ amount: e.target.value });
+  };
+
+  handleOnSubmit = e => {
+    e.preventDefault();
+    let coin = {
+      symbol: this.state.optionSelected,
+      amount: this.state.amount,
+      createdAt: Date.now()
+    };
+    if (coin) {
+      this.setState({ coins: this.state.coins.concat(coin) });
+    }
+    console.log(coin);
+  };
+
   render() {
     return (
       <div className="add-coin">
         {/* <p>Add Coin</p> */}
-        <Select className="select" options={coins} onChange={this.handleOnOptionChange} />
-        <input type="text" id="add-coin__input" placeholder="Amount" onChange={this.handleOnAmountChange}/>
+        <form onSubmit={this.handleOnSubmit}>
+          <Select className="select" options={coins} onChange={this.handleOnOptionChange} />
+          <input type="text" id="add-coin__input" placeholder="Amount" onChange=    {this.handleOnAmountChange} autoComplete="off"/>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
